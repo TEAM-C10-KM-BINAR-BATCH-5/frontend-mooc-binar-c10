9a10c7ffef244ecb2393c6d60f1dfc0ec1563444
Muhammad Nur Fajri <142598291+fajri900@users.noreply.github.com>
2023-11-28 20:04:35 +0700

    [FAJRI]add table

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,11 @@ const Navbar = () => {
     <nav className="bg-lightBlue shadow p-3">
       <div className="flex justify-between items-center">
         <div className="flex items-center">
-          <span className="text-costumeBlue text-xl font-bold">Hi, Admin</span>
+          <span className="text-costumeBlue text-xl font-bold  mx-14 py-5  ">Hi, Admin</span>
         </div>
         {/* Search Input */}
-        <div className="relative">
-          <input
-            type="text"
-            placeholder="Cari..."
-            className="bg-white border-2 border-indigo-500 rounded-lg py-2 px-6 focus:outline-none focus:border-rose-500"
-          />
+        <div className="relative mx-14">
+          <input type="text" placeholder="Cari..." className="bg-white border-2 border-indigo-500 rounded-lg py-2 px-6 focus:outline-none focus:border-rose-500 " />
           <button className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 focus:outline-none"></button>
         </div>
       </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,45 +1,41 @@
+import icons from "../assets/icon.png";
+
 export default function Dashboard() {
   return (
-    <div className="flex h-screen">
-      <main className="flex-1">
-        <h1 className="text-3xl font-bold mb-6 py-6 mx-6">Admin Dashboard</h1>
-        <p className="mb-6 py-6 mx-6">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-          nobis repellat reprehenderit, non a suscipit sunt possimus
-          consequuntur aliquid sed vel ipsam dolores error quae cum distinctio
-          aut quis nemo.
-        </p>
-        <p className="mb-6 py-6 mx-6">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-          nobis repellat reprehenderit, non a suscipit sunt possimus
-          consequuntur aliquid sed vel ipsam dolores error quae cum distinctio
-          aut quis nemo.
-        </p>
-        <p className="mb-6 py-6 mx-6">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-          nobis repellat reprehenderit, non a suscipit sunt possimus
-          consequuntur aliquid sed vel ipsam dolores error quae cum distinctio
-          aut quis nemo.
-        </p>
-        <p className="mb-6 py-6 mx-6">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-          nobis repellat reprehenderit, non a suscipit sunt possimus
-          consequuntur aliquid sed vel ipsam dolores error quae cum distinctio
-          aut quis nemo.
-        </p>
-        <p className="mb-6 py-6 mx-6">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-          nobis repellat reprehenderit, non a suscipit sunt possimus
-          consequuntur aliquid sed vel ipsam dolores error quae cum distinctio
-          aut quis nemo.
-        </p>
-        <p className="mb-6 py-6 mx-6">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-          nobis repellat reprehenderit, non a suscipit sunt possimus
-          consequuntur aliquid sed vel ipsam dolores error quae cum distinctio
-          aut quis nemo.
-        </p>
-      </main>
+    <div className="flex gap-1 m-10">
+      <div className=" bg-blue-500 rounded-2xl p-5 flex-auto border border-gray-200 flex items-center mx-3">
+        <div className="rounded-full h-12 w-12 dlex items-center justify-center bg-sky-500">
+          <img className="text-2xl text-white" src={icons} alt="" />
+        </div>
+        <div className="pl-4">
+          <span className="text-base text-white ">450</span>
+          <div className="flex items-center">
+            <strong className="text-l text-white">Active Users</strong>
+          </div>
+        </div>
+      </div>
+      <div className=" bg-green-500 rounded-2xl p-5 flex-auto border border-gray-200 flex items-center mx-3">
+        <div className="rounded-full h-12 w-12 dlex items-center justify-center bg-sky-500">
+          <img className="text-2xl text-white" src={icons} alt="" />
+        </div>
+        <div className="pl-4">
+          <span className="text-base text-white ">25</span>
+          <div className="flex items-center">
+            <strong className="text-l text-white">Active Class</strong>
+          </div>
+        </div>
+      </div>
+      <div className=" bg-indigo-600 rounded-2xl p-5 flex-auto border border-gray-200 flex items-center mx-3">
+        <div className="rounded-full h-12 w-12 dlex items-center justify-center bg-sky-500">
+          <img className="text-2xl text-white" src={icons} alt="" />
+        </div>
+        <div className="pl-4">
+          <span className="text-base text-white ">20</span>
+          <div className="flex items-center">
+            <strong className="text-l text-white">Premium Class</strong>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
