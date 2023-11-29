b3b2fed47be996944e9bd5e55218e85cd48d89e0
Agiprada <78854969+Agiprada@users.noreply.github.com>
2023-11-29 11:48:52 +0800

    [Gilang] add icon, 404 page, and refactoring card make reusable

---
+++ src/components/DataCard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import icons from "../assets/icon.png";
+
+export default function DataCard() {
+  return (
+    <div className="flex gap-1 m-10">
+      <div className=" bg-blue-500 rounded-2xl p-5 flex-auto border border-gray-200 flex items-center mx-3">
+        <div className="rounded-full h-12 w-12 dlex items-center justify-center bg-sky-500">
+          <img className="text-2xl text-white" src={icons} alt="" />
+        </div>
+        <div className="pl-4">
+          <span className="text-base text-white ">450</span>
+          <div className="flex items-center">
+            <strong className="text-l text-white">Active Users</strong>
+          </div>
+        </div>
+      </div>
+      <div className=" bg-green-500 rounded-2xl p-5 flex-auto border border-gray-200 flex items-center mx-3">
+        <div className="rounded-full h-12 w-12 dlex items-center justify-center bg-sky-500">
+          <img className="text-2xl text-white" src={icons} alt="" />
+        </div>
+        <div className="pl-4">
+          <span className="text-base text-white ">25</span>
+          <div className="flex items-center">
+            <strong className="text-l text-white">Active Class</strong>
+          </div>
+        </div>
+      </div>
+      <div className=" bg-indigo-600 rounded-2xl p-5 flex-auto border border-gray-200 flex items-center mx-3">
+        <div className="rounded-full h-12 w-12 dlex items-center justify-center bg-sky-500">
+          <img className="text-2xl text-white" src={icons} alt="" />
+        </div>
+        <div className="pl-4">
+          <span className="text-base text-white ">20</span>
+          <div className="flex items-center">
+            <strong className="text-l text-white">Premium Class</strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,47 +1,14 @@
 import icons from "../assets/icon.png";
+import DataCard from "../components/DataCard";
+import Table from "../components/Table";
 
 export default function Dashboard() {
   return (
-    <div className="flex h-screen">
-      <main className="flex-1">
-        <h1 className="text-3xl font-bold mb-6 py-6 mx-6">Admin Dashboard</h1>
-        <p className="mb-6 py-6 mx-6">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-          nobis repellat reprehenderit, non a suscipit sunt possimus
-          consequuntur aliquid sed vel ipsam dolores error quae cum distinctio
-          aut quis nemo.
-        </p>
-        <p className="mb-6 py-6 mx-6">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-          nobis repellat reprehenderit, non a suscipit sunt possimus
-          consequuntur aliquid sed vel ipsam dolores error quae cum distinctio
-          aut quis nemo.
-        </p>
-        <p className="mb-6 py-6 mx-6">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-          nobis repellat reprehenderit, non a suscipit sunt possimus
-          consequuntur aliquid sed vel ipsam dolores error quae cum distinctio
-          aut quis nemo.
-        </p>
-        <p className="mb-6 py-6 mx-6">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-          nobis repellat reprehenderit, non a suscipit sunt possimus
-          consequuntur aliquid sed vel ipsam dolores error quae cum distinctio
-          aut quis nemo.
-        </p>
-        <p className="mb-6 py-6 mx-6">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-          nobis repellat reprehenderit, non a suscipit sunt possimus
-          consequuntur aliquid sed vel ipsam dolores error quae cum distinctio
-          aut quis nemo.
-        </p>
-        <p className="mb-6 py-6 mx-6">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-          nobis repellat reprehenderit, non a suscipit sunt possimus
-          consequuntur aliquid sed vel ipsam dolores error quae cum distinctio
-          aut quis nemo.
-        </p>
-      </main>
-    </div>
+    <>
+      <DataCard />
+      <div className=" mx-10 ">
+        <Table />
+      </div>
+    </>
   );
 }
